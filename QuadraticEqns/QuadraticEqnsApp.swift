669c827e6f2b38148bc0b8faf82d2ac32baef5da
Matt Ventrelli <mventrelli@hawk.iit.edu>
2023-01-25 12:24:33 -0600

    Initial Commit

---
+++ QuadraticEqns/QuadraticEqnsApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuadraticEqnsApp.swift
+//  QuadraticEqns
+//
+//  Created by IIT PHYS 440 on 1/25/23.
+//
+
+import SwiftUI
+
+@main
+struct QuadraticEqnsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
